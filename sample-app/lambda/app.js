f4a7b4456cc160e2738f480d54dbdf7a2062b218
Michael Maeng <mmaeng@amazon.com>
2022-10-24 15:25:05 -0400

    moved folders, files out of sample-app

--- sample-app/lambda/app.js
+++
@@ -1,8 +0,0 @@
-//Handler function - change the "body" below
-exports.handler = async function(event) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: 'Hello World Application - First Branch'
-  };
-};
