5d2a77f7971a5e23e0fe2e3b1bccf0d671c7d86f
Michael Maeng <mmaeng@amazon.com>
2022-12-28 12:54:30 -0500

    Remove sample-app directory

--- sample-app/bin/cdkmultibranch.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkmultibranchStack } from '../lib/cdkmultibranch-stack';
-
-const app = new cdk.App();
-new CdkmultibranchStack(app, 'CdkmultibranchStack');
